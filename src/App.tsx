49854569634a7a5f63376fffbb8a9436ec8ea2ed
Paulo Renato <renatotravassos1@outlook.com>
2026-09-19 12:40:37 -0300

    rotas 2
    acrescentamento da rota no index principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import App_routes from './Routes/Routes'
 
 function App() {
  
   return (
      <>
-     
+<App_routes/>     
 
      
      </>
--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,9 +1,8 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link,NavLink } from 'react-router-dom';
 import Cadeiras from '../pages/Cadeiras';
 import Filmes from '../pages/Filmes';
 import Inicio from '../pages/Inicio';
-
-
+ 
 
 export default function App_routes() {
     return(        
