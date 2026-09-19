ab6e23fea026a3af5976e2805de310f15f4488dd
Paulo Renato <renatotravassos1@outlook.com>
2026-09-19 12:22:07 -0300

    rotas
    adição de rotas

---
+++ src/Routes/Routes.tsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Cadeiras from '../pages/Cadeiras';
+import Filmes from '../pages/Filmes';
+import Inicio from '../pages/Inicio';
+
+
+
+export default function App_routes() {
+    return(        
+        <BrowserRouter>
+        <nav>
+
+        <Link to="/" className='selecionado'>  ínicio</Link>  
+        <Link to="/Cadeiras">Cadeiras</Link>  
+        <Link to="/Filmes">Filmes</Link>
+
+        </nav>
+
+<Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/Cadeiras" element={<Cadeiras />} />
+        <Route path="/Filmes" element={<Filmes />} />
+        <Route path="*" element={<Inicio />} />
+
+      </Routes>        
+        </BrowserRouter>        
+    )
+}
